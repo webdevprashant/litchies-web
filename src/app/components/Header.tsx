62ea73d609430d3f34efc62f121e0e29da77092a
Prashant Kumar <prashantkumar0507200@gmail.com>
2024-05-25 23:53:23 +0530

    make header responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='m-2 h-14 header flex justify-between shadow-xl'>
+    <div className='m-2 h-14 header flex justify-between shadow-xl '>
         <div className='left'>
-            <img className='mx-2 w-30 h-12' src='images/litchieslogo.png' alt='litchies-logo' />
+            <img className='mx-2 w-30 h-12 sm: w-10/12 sm: h-5/6 md: w-8/12 md: h-5/6' src='images/litchieslogo.png' alt='litchies-logo' />
         </div>
         <div className='right m-1 py-1 flex '>
             <div className='search relative px-2' >
                 <input 
-                className="block p-2 pl-10 w-44 outline-none text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                className="block p-2 pl-10 w-11/12 sm: w-10/12 md: w-7/12 md: pl-8 outline-none text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
                 type='text' placeholder='Search' />
                 <img className='w-6 absolute top-2 left-4' src='images/search.svg' alt='Search Box' />
             </div>
             <div className='cart'>
-                <img className='w-8 h-8' src='images/shopping-cart.svg' alt='Shopping Cart' />
+                <img className='w-8 h-8 sm: w-10 md: w-12' src='images/shopping-cart.svg' alt='Shopping Cart' />
             </div>
         </div> 
     </div>
