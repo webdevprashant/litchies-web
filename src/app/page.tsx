beee154c9adec8d3fc27ca078f85630a9f4618d9
Prashant Kumar <prashantkumar0507200@gmail.com>
2024-05-28 12:57:32 +0530

    header footer fixed whole app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Category from "./components/Category";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import LikedProducts from "./components/LikedProducts";
 import PopularShops from "./components/PopularShops";
 import Shop from "./components/Shop";
 import ShopBanners from "./components/ShopBanner";
 import TrendingProducts from "./components/TrendingProducts";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
-    <Header />
     <div className="mx-2 p-8">
       <Category />
       <ShopBanners />
@@ -22,8 +19,6 @@ export default async function Home() {
       <div className="m-2 text-center">
         <button className="rounded-md w-[100%] h-10 px-14 bg-red-600 font-semibold text-white font-serif">Explore More Products</button>
       </div>
-
-      <Footer />
     </>
   );
 }
