557d36ac50ff8bd061505271ab7eff66fb163fdd
Prashant Kumar <prashantkumar69328@gmail.com>
2024-06-01 15:55:14 +0530

    add loader

---
+++ src/app/components/Loader.js
@@ -0,0 +1,23 @@
+"use client"
+import Image from "next/image";
+import React, { useEffect } from "react";
+
+const Loader = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const loader = document.getElementById('loader');
+      if (loader)
+        loader.remove();
+    }
+  }, []);
+
+  return (
+      <div className="loader" id="loader">
+        <div className="spinner">
+          <Image width={500} height={500} src="/images/loader.gif" alt="Loader Image" />
+        </div>
+      </div>
+  )
+}
+
+export default Loader;
