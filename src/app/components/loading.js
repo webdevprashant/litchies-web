2be6c65552575b5f9fd8775a8dec8ba7f23e6777
rudra <rudra@clearly.earth>
2024-06-03 15:08:22 +0530

    chore: Refactor loading component and remove unnecessary code

--- src/app/components/loading.js
+++ src/app/components/loading.js
@@ -1,44 +1,26 @@
 "use client";
 import Image from "next/image";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 const Loader = () => {
-  useEffect(() => {
-    const hideLoader = () => {
-      const loader = document.getElementById("loader");
-      if (loader) loader.style.display("hidden");
-    };
+   useEffect(() => {
+     if (typeof window !== "undefined") {
+       const loader = document.getElementById("loader");
+       if (loader) loader.remove();
+     }
+   }, []);
 
-    const showLoader = () => {
-      const loader = document.getElementById("loader");
-      if (loader) loader.style.display("block");
-    };
-
-    // Event listener for page load
-    window.addEventListener("load", hideLoader);
-
-    // Event listener for URL change (popstate)
-    window.addEventListener("popstate", showLoader);
-
-    // Cleanup function to remove event listeners
-    return () => {
-      window.removeEventListener("load", hideLoader);
-      window.removeEventListener("popstate", showLoader);
-    };
-  }, []);
-
-  return (
-    <div className="loader hidden" id="loader">
-      <div className="spinner">
-        <Image
-          width={500}
-          height={500}
-          src="/images/loader.gif"
-          alt="Loader Image"
-        />
-      </div>
-    </div>
-  );
-};
-
-export default Loader;
+   return (
+     <div className="loader" id="loader">
+       <div className="spinner">
+         <Image
+           width={500}
+           height={500}
+           src="/images/loader.gif"
+           alt="Loader Image"
+         />
+       </div>
+     </div>
+   );
+ };
+ export default Loader;
