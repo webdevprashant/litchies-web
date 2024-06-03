cc450506302bbc35f5bfad86d2a37d73e31e7e2d
rudra <rudra@clearly.earth>
2024-06-03 15:03:52 +0530

    chore: Update backend URL and add search functionality to header

--- src/app/components/loading.js
+++ src/app/components/loading.js
@@ -1,52 +1,44 @@
-"use client"
+"use client";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 const Loader = () => {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const loader = document.getElementById('loader');
-      if (loader)
-        loader.remove();
-    }
+    const hideLoader = () => {
+      const loader = document.getElementById("loader");
+      if (loader) loader.style.display("hidden");
+    };
+
+    const showLoader = () => {
+      const loader = document.getElementById("loader");
+      if (loader) loader.style.display("block");
+    };
+
+    // Event listener for page load
+    window.addEventListener("load", hideLoader);
+
+    // Event listener for URL change (popstate)
+    window.addEventListener("popstate", showLoader);
+
+    // Cleanup function to remove event listeners
+    return () => {
+      window.removeEventListener("load", hideLoader);
+      window.removeEventListener("popstate", showLoader);
+    };
   }, []);
 
   return (
-      <div className="loader" id="loader">
-        <div className="spinner">
-          <Image width={500} height={500} src="/images/loader.gif" alt="Loader Image" />
-        </div>
+    <div className="loader hidden" id="loader">
+      <div className="spinner">
+        <Image
+          width={500}
+          height={500}
+          src="/images/loader.gif"
+          alt="Loader Image"
+        />
       </div>
-  )
-}
-
-// export default function Loader() {
-//   const router = useRouter();
-
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//       const handleStart = (url) =>  setLoading(true);
-//       const handleComplete = (url) => setLoading(false);
-
-//       router.events.on('routeChangeStart', handleStart)
-//       router.events.on('routeChangeComplete', handleComplete)
-//       router.events.on('routeChangeError', handleComplete)
-
-//       return () => {
-//           router.events.off('routeChangeStart', handleStart)
-//           router.events.off('routeChangeComplete', handleComplete)
-//           router.events.off('routeChangeError', handleComplete)
-//       }
-//   })
-  
-//   return loading ? (
-//     <div className="loader" id="loader">
-//         <div className="spinner">
-//           <Image width={500} height={500} src="/images/loader.gif" alt="Loading..........." />
-//         </div>      
-//     </div>
-//     ) : null;
-// }
+    </div>
+  );
+};
 
 export default Loader;
--- src/app/products/liked/page.js
+++ src/app/products/liked/page.js
@@ -17,61 +17,67 @@ const Liked = () => {
   }, []);
   console.log(likedProducts)
   return (
-    <div className="flex flex-wrap m-2 p-4 font-serif">
-        {
-          likedProducts.map((likedProduct) => (
-            <div className="w-3/12 min-h-fit border-2 border-black"
-            key={likedProduct._id}
-            >
-
-                  {/* Row 1 */}
-                  <div className="flex m-4 justify-around">
-                        {/* Shop Bg Img */}
-                        <div className="flex justify-center bg-cover max-w-[100px] max-h-[100px]">
-                            <img className="rounded-full" src={likedProduct.shopId.shopBgThumbnail} alt="Shop Background Image" />
-                        </div>
-
-                        {/* Shop name, description */}
-                        <div className="truncate mx-2">
-                            <p>{likedProduct?.shopId?.name}</p>
-                            <p>{likedProduct?.shopId?.desc}</p>
-                        </div>
-
-                        {/* shop ratings */}
-                        <div className="">
-                            <p>RATINGS</p>
-                            <p>{likedProduct?.shopId?.ratings} ⭐</p>
-                        </div>
-
-
-                  </div>
-
-                  {/* Row 2 (Product square image, iccons*/ }
-                  <div className="flex justify-between m-4">
-                          <div className="bg-cover w-3/5 max-h-[200px]">
-                            <img className="rounded-2xl bg-cover" src={likedProduct.thumbnailURL} />
-                          </div>
-
-                          <div className="flex flex-col justify-around">
-                          <BiLike size={30} />
-                          <CiHeart size={30} />
-                          <FaWhatsapp size={30} />
-                          <RiShareForward2Fill size={30} />
-                          </div>
-                  </div>
-
-                  {/* Row 3 */}
-                  <div className="flex justify-between m-4">
-                        <h4>{likedProduct?.name}</h4>
-                        <p>Rs. {likedProduct?.price}</p>
-                  </div>
+    <div className="flex flex-wrap justify-center m-2 p-6 font-serif">
+      {likedProducts.map((likedProduct) => (
+        <div
+          className="w-[400px] min-h-fit border marginLeft-2 m-2 p-2 rounded-lg shadow-md hover:shadow-lg "
+          key={likedProduct._id}
+        >
+          {/* Row 1 */}
+          <div className="flex m-4 justify-between items-center">
+            {/* Shop Bg Img */}
+            <div className="flex justify-center bg-cover">
+              <img
+                className="rounded-full w-[50px] h-[50px] bg-contain"
+                src={likedProduct.shopId.shopBgThumbnail}
+                alt="Shop Background Image"
+              />
             </div>
-          ))
-        }
 
-    
+            {/* Shop name, description */}
+            <div className="w-8/12 mx-2">
+              <p>{likedProduct?.shopId?.name}</p>
+              <p
+                className=" h-10 overflow-hidden"
+                style={{ fontWeight: 50, fontSize: "smaller" }}
+              >
+                {likedProduct?.shopId?.address}
+              </p>
+            </div>
+
+            {/* shop ratings */}
+            <div className="w-[50px]">
+              <p>RATINGS</p>
+              <p>{likedProduct?.shopId?.ratings} ⭐</p>
+            </div>
+          </div>
+
+          {/* Row 2 (Product square image, iccons*/}
+          <div className="flex justify-between m-4">
+            <div className="">
+              <img
+                className="rounded-2xl bg-contain h-[250px] w-[250px] bg-cover"
+                src={likedProduct.thumbnailURL}
+              />
+            </div>
+
+            <div className="flex flex-col justify-around">
+              <BiLike size={30} />
+              <CiHeart size={30} />
+              <FaWhatsapp size={30} />
+              <RiShareForward2Fill size={30} />
+            </div>
+          </div>
+
+          {/* Row 3 */}
+          <div className="flex justify-between m-4">
+            <h4>{likedProduct?.name}</h4>
+            <p>Rs. {likedProduct?.price}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Liked;
