3d3b9cae9956f9a45086db7791cda67ab4333430
Prashant Kumar <prashantkumar69328@gmail.com>
2024-06-17 17:54:27 +0530

    profile page redirection in case of already login

--- src/app/components/home/Footer.js
+++ src/app/components/home/Footer.js
@@ -6,16 +6,18 @@ import { IoMdHome } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
 
 const Footer = () => {
+  const userRegister = useSelector((store) => store.storeOTP.isUserRegistered);
   const [selectedIcon, setSelectedIcon] = useState(3);
   const icons = [
     { id: 1, component: <MdOutlineVideoLibrary size={30} /> },
     { id: 2, component: <IoIosNotifications size={30} /> },
     { id: 3, component: <Link href="/"><IoMdHome size={30} /></Link> },
     { id: 4, component: <FaHeart size={30} /> },
-    { id: 5, component: <Link href="/profile"><FaRegUser size={30} /></Link> },
+    { id: 5, component: <Link href={ userRegister ? "/profile/me" :  "/profile" }><FaRegUser size={30} /></Link> },
   ];
 
   const handleIconClick = (id) => {
