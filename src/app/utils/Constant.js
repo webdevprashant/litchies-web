61cfe32a98050ca4298dd791fccd80c2363a031d
Prashant Kumar <prashantkumar69328@gmail.com>
2024-06-20 14:47:01 +0530

    production deploy

--- src/app/utils/Constant.js
+++ src/app/utils/Constant.js
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = "http://api.litchies.com:5000";
-export const BACKEND_URL="http://localhost:5000";
+export const BACKEND_URL = "http://api.litchies.com:5000";
+// export const BACKEND_URL="http://localhost:5000";
 export const PAGE = 1;
 export const COUNT = 10;
